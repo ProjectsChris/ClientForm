67521d2807378a43d6668fc5434123fe011dfce7
Chris Deboli <christopher2002buffon@gmail.com>
2020-12-17 14:48:16 +0100

    Aggiungere i file di progetto.

---
+++ ClientForm/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace ClientForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txt_ipserver = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_porta = new System.Windows.Forms.TextBox();
+            this.btn_Connetti = new System.Windows.Forms.Button();
+            this.lst_messaggi = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_messaggio = new System.Windows.Forms.TextBox();
+            this.btn_invia = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_ipserver
+            // 
+            resources.ApplyResources(this.txt_ipserver, "txt_ipserver");
+            this.txt_ipserver.Name = "txt_ipserver";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // txt_porta
+            // 
+            resources.ApplyResources(this.txt_porta, "txt_porta");
+            this.txt_porta.Name = "txt_porta";
+            // 
+            // btn_Connetti
+            // 
+            resources.ApplyResources(this.btn_Connetti, "btn_Connetti");
+            this.btn_Connetti.Name = "btn_Connetti";
+            this.btn_Connetti.UseVisualStyleBackColor = true;
+            this.btn_Connetti.Click += new System.EventHandler(this.btn_Connetti_Click);
+            // 
+            // lst_messaggi
+            // 
+            this.lst_messaggi.FormattingEnabled = true;
+            resources.ApplyResources(this.lst_messaggi, "lst_messaggi");
+            this.lst_messaggi.Name = "lst_messaggi";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // txt_messaggio
+            // 
+            resources.ApplyResources(this.txt_messaggio, "txt_messaggio");
+            this.txt_messaggio.Name = "txt_messaggio";
+            // 
+            // btn_invia
+            // 
+            resources.ApplyResources(this.btn_invia, "btn_invia");
+            this.btn_invia.Name = "btn_invia";
+            this.btn_invia.UseVisualStyleBackColor = true;
+            this.btn_invia.Click += new System.EventHandler(this.btn_invia_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.btn_invia);
+            this.Controls.Add(this.txt_messaggio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lst_messaggi);
+            this.Controls.Add(this.btn_Connetti);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_porta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_ipserver);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_ipserver;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_porta;
+        private System.Windows.Forms.Button btn_Connetti;
+        private System.Windows.Forms.ListBox lst_messaggi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_messaggio;
+        private System.Windows.Forms.Button btn_invia;
+    }
+}
+
---
+++ ClientForm/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ClientForm
+{
+    public partial class Form1 : Form
+    {
+        // Creo il socket client
+        Socket client;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Assegno il socket al client
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        private void btn_Connetti_Click(object sender, EventArgs e)
+        {
+            // Creo le variabili per inserire l'indirizzo IP del server
+            IPAddress ipAddress = null;
+            int nPort = 0;
+
+            // Provo a convertire l'indirizzo IP
+            if (!IPAddress.TryParse(txt_ipserver.Text, out ipAddress))
+            {
+                MessageBox.Show("Indirizzo IP errato", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error); // Avviso all'utente dell'errore
+                return;
+                txt_ipserver.Text = null; // Pulisco la textbox
+                txt_ipserver.Focus();
+            }
+
+            // Provo a convertire il numero di porta
+            if (!int.TryParse(txt_porta.Text, out nPort))
+            {
+                MessageBox.Show("Numeri porta errato", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error); // Avviso all'utente dell'errore
+                return;
+                txt_porta.Focus();
+                txt_porta.Text = null; // Pulisco la textbox
+            }
+
+            // Controllo se il numero di porta è errato
+            if (nPort <= 0 || nPort >= 65535)
+            {
+                MessageBox.Show("Numero porta errato", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error); // Avviso all'utente dell'errore
+                return;
+                txt_porta.Focus();
+                txt_porta.Text = null; // Pulisco la textbox
+            }
+
+            try // Faccio la try/catch per evitare che l'errore mi fa chiudere il programma
+            {
+                // Connetto il client con il server
+                client.Connect(ipAddress, nPort);
+
+                // Controllo se il client si è connesso definivamente
+                if (client.Connected)
+                {
+                    btn_Connetti.Text = "Connesso"; // Cambio il testo del button
+                    txt_messaggio.Focus();
+
+                    // Abilito la textbox e il button
+                    txt_messaggio.Enabled = true;
+                    btn_invia.Enabled = true;
+
+                    // Disabilito le textbox e il button
+                    txt_ipserver.Enabled = false;
+                    txt_porta.Enabled = false;
+                    btn_Connetti.Enabled = false;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Server non trovato", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Information); // Avviso all'utente dell'errore
+            }
+        }
+
+        private void btn_invia_Click(object sender, EventArgs e)
+        {
+            // Dichiarazione delle variabili
+            byte[] sendbuffer = new byte[128]; // array di byte da inviare al server
+            byte[] recvbuffer = new byte[128]; // array di byte da ricevere dal server
+            string recvString = ""; // string per avere il testo del messaggio dal server
+            int recvBytes = 0; // quantità di byte che contiene un messaggio dal server
+
+            // Controllo se il client è connesso
+            if (client.Connected)
+            {
+                // Controllo se la casella di testo non è vuota
+                if (txt_messaggio.Text != "")
+                {
+                    // Controllo se nella casella di testo non contiene la parola "quit"
+                    if (txt_messaggio.Text.ToUpper().Trim() != "QUIT")
+                    {
+                        // Invio il messaggio al server
+                        sendbuffer = Encoding.ASCII.GetBytes(txt_messaggio.Text); // Converto il messaggio in byte per poi trasferirli nel buffer
+                        client.Send(sendbuffer); // Invio il buffer al server
+                        txt_messaggio.Focus();
+                        Array.Clear(recvbuffer, 0, recvbuffer.Length); // Pulisco l'array di byte inviati al server
+
+                        // Ricevo il messaggio dal server
+                        recvBytes = client.Receive(recvbuffer); // Ricevo il buffer dal server e trasferisco la quantità di bytes nella variabile
+                        recvString = Encoding.ASCII.GetString(recvbuffer, 0, recvBytes); // Converto il messaggio del server da byte in string
+                        lst_messaggi.Items.Add("S: " + recvString); // Stampo la stringa del server
+                        txt_messaggio.Text = null; // Pulisco la textbox
+                    }
+                    else
+                    {
+                        client.Shutdown(SocketShutdown.Both); // Disabilito la connessione del client
+                        client.Close(); // Chiudo la connessione
+                        client.Dispose(); // Rilascia le risorse non gestite
+                        lst_messaggi.Items.Clear(); // Pulisco la listbox
+                        txt_ipserver.Text = null; // Pulisco la textbox
+                        txt_messaggio.Text = null; // Pulisco la textbox
+                        txt_porta.Text = null; // Pulisco la textbox
+                        txt_ipserver.Focus();
+                        btn_Connetti.Text = "Connetti";
+
+                        // Disabilito la textbox e il button
+                        txt_messaggio.Enabled = false;
+                        btn_invia.Enabled = false;
+
+                        // Abilito le textbox e il button
+                        txt_ipserver.Enabled = true;
+                        txt_porta.Enabled = true;
+                        btn_Connetti.Enabled = true;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Devi inserire un messaggio", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Information); // Avviso all'utente dell'errore
+                    txt_messaggio.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Client non è connesso", "Attenzione", MessageBoxButtons.OK, MessageBoxIcon.Information); // Avviso all'utente dell'errore
+            }
+        }
+    }
+}
